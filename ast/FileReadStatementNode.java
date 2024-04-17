e7c1fe79edb27b556b3c5388c3e22f4434ca52a8
ATHOMAS001 <athomas001@vplincsci073.ad.csbsju.edu>
2024-04-17 08:53:10 -0500

    Updating FileReadStatementNode so it throws Exception

--- ast/FileReadStatementNode.java
+++ ast/FileReadStatementNode.java
@@ -1,4 +1,5 @@
 import java_cup.runtime.*;
+import java.util.*;
 
 /**
  * This class represents a file read statement in the AST.
@@ -13,5 +14,19 @@ public class FileReadStatementNode extends AssignStatementNode {
     @Override
     public String toString() {
         return "File Read into variable: " + this.getId();
+    }
+    
+    /**
+     *This method impliments the run method all statements need to
+     *@param symbolTableList list with the symbol tables in order from least
+     *to most specific
+     *@param functionTable map with the functions
+     *@throws UnsupportedOperationException not yet implemented
+     */
+    @Override
+    public void run(List<Map<String, TypeExpressionPair>> symbolTableList,
+            Map<String, FunctionNode> functionTable){
+        //TODO: Implement run
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
