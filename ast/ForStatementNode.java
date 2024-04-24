d7b02f04844cbb338f6360b46caab7064c84f79f
ATHOMAS001 <athomas001@vplincsci071.ad.csbsju.edu>
2024-04-24 12:33:47 -0500

    Working on for loop. Still not perfect, getting error running test 13, but it compiles

--- ast/ForStatementNode.java
+++ ast/ForStatementNode.java
@@ -54,22 +54,40 @@ public class ForStatementNode extends StatementNode {
 		throw new IllegalStateException("Cannot re-instantiate variable: " + this.loopVar);	
 	    }
 	}
-
-	context = symbolTableList.get(symbolTableList.size() - 1);
-	if (!(context.get(this.collection).getType() instanceof ListTypeNode)){ //Checks that the collection's type is a ListType (which has a child node containing the element type). We do this first to make sure we don't get a NullPointer when we try to get the child in the next check
+    this.collection = this.collection.evaluate(symbolTableList, functionTable);    
+	if (!(this.collection instanceof ListExpressionNode)){ //Checks that the collection's type is a ListType (which has a child node containing the element type). We do this first to make sure we don't get a NullPointer when we try to get the child in the next check
 	    throw new IllegalArgumentException("Must give list variable in for loop");
 	}
+    //The for loop should have its own context
+    context = new HashMap<String, TypeExpressionPair>();
+    symbolTableList.add(context);
+    context.put(this.loopVar, new TypeExpressionPair(this.loopVarType, null));
 
-	TypeNode listType = context.get(collection).getType();
-	if (listType.children.get(0) != this.loopVarType){ //Checks that the collection's element type is the same as the Loop Variable's Type
-	    throw new IllegalArgumentException("List Type must match Loop Variable Type");   
-	}
+
 	ListExpressionNode listExpr = (ListExpressionNode) this.collection.evaluate(symbolTableList,functionTable); //I could put this in a try, and catch TypeCastExceptions, but we check above to make sure collection is a List already so I don't know if that would be redundant / necessary 
 	for (int i = 0; i < listExpr.size(); i++){
-	    //context.put(this.loopVar, listExpr.get(i)); Need to make the listExpression into a TypeExpressionPair to put it into the context
-	    //We have a loop that will set the loopVar to successive entries in the List Expression in the current context
-	    //TODO: Put context back into larger SymbolTableList then call Interpreter.runStatementList() with this.body for the statement list
+        TypeExpressionPair loopPair = context.get(this.loopVar);
+        switch (loopPair.getType().getTypeName()){
+            case "num":
+                loopPair.setValue(new NumExpressionNode((Double)listExpr.get(i)));
+                break;
+
+            case "text":
+                loopPair.setValue(new StringExpressionNode((String)listExpr.get(i)));
+                break;
+
+            case "bool":
+                loopPair.setValue(new BooleanLiteralExpressionNode((Boolean)listExpr.get(i)));
+                break;
+
+            default:
+                symbolTableList.remove(context);
+                throw new IllegalStateException("Error iterating through list");
+
+        }
+        Interpreter.runStatementList(symbolTableList, functionTable, this.body); 
 	}
+        symbolTableList.remove(context);
         throw new UnsupportedOperationException("Types are correct, for loop not yet implemented");
     }
 }
