8de686f5c48a0f6e5a2fafe9dc5024922e6918be
ATHOMAS001 <athomas001@vplincsci027.ad.csbsju.edu>
2024-04-27 16:14:27 -0500

    Whoops last commit was removing .expected files in tests, this is for Until StatementNode

--- README.md
+++ README.md
@@ -3,7 +3,4 @@ Interpreter for the language we made in class
 Run "make Interpreter.class" to compile the Interpreter class and "./interpreter {FILENAME}" to run it on a file
 This is to make sure the classpath is correct
 
-4/10/24 - Interpreter class exists and retrieves the AST for the given program
-
-4/26/24 Doesn't pass: 
-test8 (UntilStatement not implemented)
+4/27/24: Should pass all tests 
--- ast/UntilStatementNode.java
+++ ast/UntilStatementNode.java
@@ -33,7 +33,36 @@ public class UntilStatementNode extends StatementNode {
     @Override
     public void run(List<Map<String, TypeExpressionPair>> symbolTableList,
             Map<String, FunctionNode> functionTable){
-        //TODO: Implement run
-        throw new UnsupportedOperationException("Not yet implemented");
+        boolean crash = true;
+        List<Map<String, TypeExpressionPair>> duplicate = null;
+        while(crash){
+            duplicate = deepCopy(symbolTableList);
+            try{
+                this.checkStatement.run(duplicate, functionTable);
+                crash = false;
+            }catch(Exception e){
+                Interpreter.runStatementList(symbolTableList, functionTable, this.loopBody);
+            }
+        }
+        for(int i = 0; i < symbolTableList.size(); i++){
+            symbolTableList.set(i, duplicate.get(i));
+        }   
+
+    }
+
+    private List<Map<String, TypeExpressionPair>> deepCopy(List<Map<String, TypeExpressionPair>> toBeCopied){
+        List<Map<String, TypeExpressionPair>> newList = new ArrayList<Map<String, TypeExpressionPair>>();
+        for(int i = 0; i < toBeCopied.size(); i++){
+            newList.add(deepEnoughCopy(toBeCopied.get(i)));
+        }
+        return newList;
+    }
+
+    private Map<String, TypeExpressionPair> deepEnoughCopy(Map<String, TypeExpressionPair> toBeCopied){
+        Map<String, TypeExpressionPair> newMap = new HashMap<String, TypeExpressionPair>();
+        for(String key: toBeCopied.keySet()){
+            newMap.put(key, toBeCopied.get(key));
+        }
+        return newMap;
     }
 }
